7828f4dec29c96b8fc9e59de6308204041f06081
廖宣雅 <angela851109tall@gmail.com>
2018-09-27 11:00:44 +0800

    更新

--- week_2/HW_2_UrCosme_crawler/UrCosme_crawler.R
+++ week_2/HW_2_UrCosme_crawler/UrCosme_crawler.R
@@ -21,29 +21,38 @@ ranking_cosmetic <- read_html(html_li)
 product_list <- ranking_cosmetic %>% 
   html_nodes(".single-dot") %>% 
   html_text()
-length(product_list)
+
 product_list
+product_list_new <- c()
+for (i in 1:length(product_list)) {
+  if (i %% 2 == 1) {
+    product_list_new <- c(product_list_new, product_list[i])
+  }
+}
+length(product_list_new)
 
-product_list <- paste0(".single-dot" , product)
-View(product_list)
+product_list_new <- paste0(".single-dot" , product_list)
+View(product_list_new)
 
-# 抓價錢、日期
+# 抓價錢
 money_list <- ranking_cosmetic %>% 
-  html_nodes(".product-market-date , span+ span") %>%
+  html_nodes(".uc-tag-ranking-item:nth-child(11) span , .uc-tag-ranking-item:nth-child(9) span , .uc-tag-ranking-item:nth-child(5) span , .uc-tag-ranking-item:nth-child(2) span , span~ span+ span") %>%
   html_text()
-length(money_list)
+
 money_list
 
-##去除資料  
-#money_list<-gsub(" "｜" "","",money_list)
+length(money_list)
 
-money_list <- paste0("product-market-date , span+ span" , money)
-View(money_list)
+##去除資料  
+#money_list<-gsub("｜",  "," ,money_list[127])
+
+#money_list <- paste0("product-market-date , span+ span" , money)
+#View(money_list)
 
 
 # 評價
 score_list <- ranking_cosmetic %>% 
-  html_nodes(".product-score-text , .brand-name ") %>%
+  html_nodes(".urcosme-heart-full~ .product-score-text") %>%
   html_text()
 as.numeric()
 length(score_list)
@@ -54,28 +63,29 @@ poster_list <- ranking_cosmetic %>%
   html_nodes(".img-auto-center ") %>%
   html_attr("src")
 poster_list
+length(poster_list)
 
-#news_li <- data.frame("評價"= score, "產品"= product, "價錢、日期"=money, "圖片"=poster)
+#news_li <- data.frame("評價"= score, "產品"= product, "價錢"=money, "圖片"=poster)
 #View(news_li)
 #paste(product_list, money_list, score_list,sep="")
 
 ##長度不同不能合起來
-colnames(df_cosmetic)<-c("產品","價格","分數","圖片")
-df_cosmetic = data.frame(product_list, money_list, score_list)
-df_cosmetic
+#colnames(df_cosmetic)<-c("產品","價格","分數","圖片")
+#df_cosmetic = data.frame(product_list, money_list, score_list)
+#df_cosmetic
 
 
 
 #排序 評價 產品 價位 圖片 列成一列
 
-x <- cbind(c(product), c(money))
-y <- cbind(c(product), c(score))
+#x <- cbind(c(product), c(money))
+#y <- cbind(c(product), c(score))
 #z <- cbind(c(product), c(poster))
-colnames(x) <- c("product", "money")
-colnames(y) <- c("product", "score")
+#colnames(x) <- c("product", "money")
+#colnames(y) <- c("product", "score")
 #colnames(z) <- c("product", "poster")
 
-merge(x, y , by = "product", all = T) 
+#merge(x, y , by = "product", all = T) 
 
 
  
